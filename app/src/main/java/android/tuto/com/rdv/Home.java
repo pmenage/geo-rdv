c40d9548739e79148bc88c356b3c751879429bba
kbouaddi <khadija.bouaddi1@gmail.com>
2017-12-05 16:19:40 +0100

    Added request permissions

--- app/src/main/java/android/tuto/com/rdv/Home.java
+++ app/src/main/java/android/tuto/com/rdv/Home.java
@@ -1,21 +1,22 @@
 package android.tuto.com.rdv;
 
+import android.Manifest;
 import android.app.Activity;
-import android.app.PendingIntent;
-import android.content.BroadcastReceiver;
-import android.content.Context;
 import android.content.Intent;
-import android.content.IntentFilter;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.telephony.SmsManager;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
 
 public class Home extends Activity {
-
+    private  static final int SMS_PERMISSION_CODE=1;
+    // button sendSMS
     Button invite;
+    // button editText
     EditText num;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,85 +26,59 @@ public class Home extends Activity {
         invite = (Button) findViewById(R.id.invite);
         num = (EditText) findViewById(R.id.num);
 
-        invite.setOnClickListener(new View.OnClickListener()
-        {
-            public void onClick(View v)
-            {
-                String phoneNo = num.getText().toString();
-               // String message = txtMessage.getText().toString();
-                  String message="Je vous invite à un RDV";
-                if (phoneNo.length()>0 && message.length()>0)
-                    sendRDV(phoneNo,message);
-                else
-                    Toast.makeText(getBaseContext(),
-                            "Please enter  phone number ",
-                            Toast.LENGTH_SHORT).show();
+        invite.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public  void onClick(View v){
+                String myMessage="hello this is my first msg";
+                String number=num.getText().toString();
+                sendMsg(number,myMessage);
             }
+
         });
+
     }
 
-    private void sendRDV(String phoneNumber,String message){
-        String SENT = "SMS_SENT";
-        String DELIVERED = "SMS_DELIVERED";
+    public boolean isSmsPermissionGranted() {
+        return ContextCompat.checkSelfPermission(this, Manifest.permission.READ_SMS) == PackageManager.PERMISSION_GRANTED;
+    }
 
-        PendingIntent sentPI = PendingIntent.getBroadcast(this, 0,
-                new Intent(SENT), 0);
 
-        PendingIntent deliveredPI = PendingIntent.getBroadcast(this, 0,
-                new Intent(DELIVERED), 0);
+    private void requestReadAndSendSmsPermission() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_SMS)) {
+            // You may display a non-blocking explanation here, read more in the documentation:
+            // https://developer.android.com/training/permissions/requesting.html
+        }
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_SMS},SMS_PERMISSION_CODE);
+    }
 
-        //---when the SMS has been sent---
-        registerReceiver(new BroadcastReceiver(){
-            @Override
-            public void onReceive(Context arg0, Intent arg1) {
-                switch (getResultCode())
-                {
-                    case Activity.RESULT_OK:
-                        Toast.makeText(getBaseContext(), "SMS sent",
-                                Toast.LENGTH_SHORT).show();
-                        break;
-                    case SmsManager.RESULT_ERROR_GENERIC_FAILURE:
-                        Toast.makeText(getBaseContext(), "Generic failure",
-                                Toast.LENGTH_SHORT).show();
-                        break;
-                    case SmsManager.RESULT_ERROR_NO_SERVICE:
-                        Toast.makeText(getBaseContext(), "No service",
-                                Toast.LENGTH_SHORT).show();
-                        break;
-                    case SmsManager.RESULT_ERROR_NULL_PDU:
-                        Toast.makeText(getBaseContext(), "Null PDU",
-                                Toast.LENGTH_SHORT).show();
-                        break;
-                    case SmsManager.RESULT_ERROR_RADIO_OFF:
-                        Toast.makeText(getBaseContext(), "Radio off",
-                                Toast.LENGTH_SHORT).show();
-                        break;
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case SMS_PERMISSION_CODE: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // permission was granted, yay! Do the
+                    // SMS related task you need to do.
+
+                } else {
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
                 }
+                return;
             }
-        }, new IntentFilter(SENT));
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
+    }
 
-        //---when the SMS has been delivered---
-        registerReceiver(new BroadcastReceiver(){
-            @Override
-            public void onReceive(Context arg0, Intent arg1) {
-                switch (getResultCode())
-                {
-                    case Activity.RESULT_OK:
-                        Toast.makeText(getBaseContext(), "SMS delivered",
-                                Toast.LENGTH_SHORT).show();
-                        break;
-                    case Activity.RESULT_CANCELED:
-                        Toast.makeText(getBaseContext(), "SMS not delivered", Toast.LENGTH_SHORT).show();
-                        break;
-                }
-            }
-        }, new IntentFilter(DELIVERED));
+    protected void sendMsg(String number, String message) {
+        SmsManager sms=SmsManager.getDefault();
+        sms.sendTextMessage(number,null,message,null,null);
 
-        SmsManager sms = SmsManager.getDefault();
-        sms.sendTextMessage(phoneNumber, null, message, sentPI, deliveredPI);
- }
-
-
+    }
  //----------------------------------liste des contacts----------------------------------------
     public void getList(View v){
 
