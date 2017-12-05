b1ee9cd466b8b7b8638eff10c1f8c605eb0ef108
kbouaddi <khadija.bouaddi1@gmail.com>
2017-12-05 14:25:18 +0100

    Added initial files

--- app/src/main/java/android/tuto/com/rdv/Home.java
+++ app/src/main/java/android/tuto/com/rdv/Home.java
@@ -1,26 +1,115 @@
 package android.tuto.com.rdv;
 
+import android.app.Activity;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.telephony.SmsManager;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class Home extends AppCompatActivity {
+public class Home extends Activity {
 
+    Button invite;
+    EditText num;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        invite = (Button) findViewById(R.id.invite);
+        num = (EditText) findViewById(R.id.num);
+
+        invite.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View v)
+            {
+                String phoneNo = num.getText().toString();
+               // String message = txtMessage.getText().toString();
+                  String message="Je vous invite à un RDV";
+                if (phoneNo.length()>0 && message.length()>0)
+                    sendRDV(phoneNo,message);
+                else
+                    Toast.makeText(getBaseContext(),
+                            "Please enter  phone number ",
+                            Toast.LENGTH_SHORT).show();
+            }
+        });
     }
+
+    private void sendRDV(String phoneNumber,String message){
+        String SENT = "SMS_SENT";
+        String DELIVERED = "SMS_DELIVERED";
+
+        PendingIntent sentPI = PendingIntent.getBroadcast(this, 0,
+                new Intent(SENT), 0);
+
+        PendingIntent deliveredPI = PendingIntent.getBroadcast(this, 0,
+                new Intent(DELIVERED), 0);
+
+        //---when the SMS has been sent---
+        registerReceiver(new BroadcastReceiver(){
+            @Override
+            public void onReceive(Context arg0, Intent arg1) {
+                switch (getResultCode())
+                {
+                    case Activity.RESULT_OK:
+                        Toast.makeText(getBaseContext(), "SMS sent",
+                                Toast.LENGTH_SHORT).show();
+                        break;
+                    case SmsManager.RESULT_ERROR_GENERIC_FAILURE:
+                        Toast.makeText(getBaseContext(), "Generic failure",
+                                Toast.LENGTH_SHORT).show();
+                        break;
+                    case SmsManager.RESULT_ERROR_NO_SERVICE:
+                        Toast.makeText(getBaseContext(), "No service",
+                                Toast.LENGTH_SHORT).show();
+                        break;
+                    case SmsManager.RESULT_ERROR_NULL_PDU:
+                        Toast.makeText(getBaseContext(), "Null PDU",
+                                Toast.LENGTH_SHORT).show();
+                        break;
+                    case SmsManager.RESULT_ERROR_RADIO_OFF:
+                        Toast.makeText(getBaseContext(), "Radio off",
+                                Toast.LENGTH_SHORT).show();
+                        break;
+                }
+            }
+        }, new IntentFilter(SENT));
+
+        //---when the SMS has been delivered---
+        registerReceiver(new BroadcastReceiver(){
+            @Override
+            public void onReceive(Context arg0, Intent arg1) {
+                switch (getResultCode())
+                {
+                    case Activity.RESULT_OK:
+                        Toast.makeText(getBaseContext(), "SMS delivered",
+                                Toast.LENGTH_SHORT).show();
+                        break;
+                    case Activity.RESULT_CANCELED:
+                        Toast.makeText(getBaseContext(), "SMS not delivered", Toast.LENGTH_SHORT).show();
+                        break;
+                }
+            }
+        }, new IntentFilter(DELIVERED));
+
+        SmsManager sms = SmsManager.getDefault();
+        sms.sendTextMessage(phoneNumber, null, message, sentPI, deliveredPI);
+ }
+
+
+ //----------------------------------liste des contacts----------------------------------------
     public void getList(View v){
 
         Intent intent = new Intent(this,MyListActivity.class);
         startActivity(intent);
     }
 
-   /* public void addContact(View v){
-        Intent intent = new Intent(this,AddContactActivity.class);
-        startActivity(intent);
 
-    }*/
 }
